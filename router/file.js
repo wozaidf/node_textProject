8da7cb51b84f2c23c850c97bf364a0b206f531b7
wozaidf <2217957466@qq.com>
2022-11-02 18:31:40 +0800

    第二天

--- router/file.js
+++ router/file.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const file_handle = require('../router_handle/file');
+const path = require('path')
 // 解析form-data
-const multipart = require('connect-multiparty');
-const multipartyMiddleware = multipart();
+const multer = require('multer')
 
-router.post('/uploadFile', multipartyMiddleware, file_handle.uploadFile)
+// const storage = multer.diskStorage({
+//     //保存路径
+//     destination: function (req, file, cb) {
+//         cb(null, path.join(__dirname,'/file'))
+//         //注意这里的文件路径,不是相对路径，直接填写从项目根路径开始写就行了
+//     },
+//     //保存在 destination 中的文件名
+//     filename: function (req, file, cb) {
+//         cb(null, file.fieldname + '-' + Date.now())
+//     }
+// })
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+router.post('/uploadFile', upload.single('file'), file_handle.uploadFile)
 
 module.exports = router
--- router_handle/file.js
+++ router_handle/file.js
@@ -1,6 +1,21 @@
 const uploadFile = (req, res) => {
-    res.send('ok');
-    console.log(req.files);
+    // // 整理数据
+    // console.log(req.file);
+    // const uploadFile = req.file;
+    // if (uploadFile.size < 1024) {
+    //     uploadFile.size = Math.ceil(uploadFile.size) + "B";
+    //  } else if (uploadFile.size / 1024 < 1024) {
+    //     uploadFile.size = Math.ceil(uploadFile.size / 1024) + "KB";
+    //  } else if (uploadFile.size / 1024 / 1024 < 5) {
+    //     uploadFile.size = Math.ceil(uploadFile.size / 1024 / 1024) + "MB";
+    //  }
+    //  // 生成现在上传的时间
+    //  console.log(uploadFile.size);
+    //  const date = new Date().toLocaleString();
+    //  uploadFile.time = date;
+    // console.log(uploadFile);
+    console.log(req.file);
+    res.send('ok')
 }
 
 module.exports = {
