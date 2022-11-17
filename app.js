400d3030733df35077639746f5f15b7fabb3f097
wozaidf <2217957466@qq.com>
2022-11-17 20:36:44 +0800

    完成

--- app.js
+++ app.js
@@ -30,13 +30,13 @@ app.use(express.urlencoded({ extended: false }));
 
 // 定义将JWT字符串解析还原成JSON对象的中间件
 // 就会自动的将解析出来的用户信息挂载到req.auth属性上，unless表示不需要权限的接口
-app.use(jwt({ secret: secretKey, algorithms: ["HS256"] }).unless({ path: [/^\/user\//, /^\/images\//] }));
+app.use(jwt({ secret: secretKey, algorithms: ["HS256"] }).unless({ path: [/^\/user\//, /^\/images\//, /^\/agreement\//] }));
 // 注册路由
 app.use('/user', userRouter);
 app.use('/file', fileRouter);
 app.use('/images', imageRouter);
 app.use('/images', express.static(path.join(__dirname, './public/images')));
-console.log(path.join(__dirname, './public/images'))
+app.use('/agreement',express.static('./public'))
 // 定义错误级别中间件
 app.use((err, req, res, next) => {
     // 验证用户输入信息失败导致的错误
